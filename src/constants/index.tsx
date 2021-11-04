fa9d0a9bea684d719795a2b58e74eba03309b3a1
Administrator <gfmhosting@gmail.com>
2021-11-04 13:34:51 +0000

    Update index.tsx Contacts Edit

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,6 @@
-export const MOBILE_PHONE = '79268007207';
-export const MOBILE_PHONE_BEAUTY = '+7 (926) 800 72 07';
-export const TELEGRAM = 'fotozona77';
-export const INSTAGRAM_LINK = 'https://www.instagram.com/fotozona77';
-export const EMAIL = 'info@fotozona77.ru';
+export const MOBILE_PHONE = '';
+export const MOBILE_PHONE_BEAUTY = '';
+export const TELEGRAM = '';
+export const INSTAGRAM_LINK = 'https://www.instagram.com/#';
+export const EMAIL = 'info@vyezdnoy-garderob77.ru';
 export const ADDRESS = 'г. Москва, пр. Егорьевский, д.2а, стр.10';
