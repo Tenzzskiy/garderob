9a38d7f455dd3bed983352d8ff97f82bb093e420
Administrator <gfmhosting@gmail.com>
2021-11-28 10:45:32 +0000

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,27 @@
 import {Props} from './layout.props';
 import {HeaderSection, FooterSection, ContactSection} from '@/components';
+import {useEffect} from 'react';
 
 const Layout = ({children}: Props): JSX.Element => {
+	useEffect(() => {
+		loadScript('/main.js');
+	});
 	return (
 		<>
 			<HeaderSection />
 			{children}
 			<ContactSection />
 			<FooterSection />
+			<noscript>
+				<div>
+					<img
+						src='https://mc.yandex.ru/watch/86277172'
+						style={{position: 'absolute', left: '-9999px'}}
+						alt=''
+					/>
+				</div>
+			</noscript>
+
 		</>
 	);
 };
