0dff31ac5b3313276a6a565063ae4bad66e7808c
Rasif Rustamov <19rustamov1996@gmail.com>
2021-12-10 12:53:32 +0300

    Add new photos

--- src/components/pages/search/SearchPage/searchPage.tsx
+++ src/components/pages/search/SearchPage/searchPage.tsx
@@ -2,15 +2,19 @@ import styles from './searchPage.module.scss';
 import classNames from 'classnames';
 import {Props} from './searchPage.props';
 import {GarderobCard} from '@/components';
+import useAppSelector from '@/hooks/useAppSelector';
 
 function SearchPage({items}: Props): JSX.Element {
+	const allItems = useAppSelector(state => state.shopState.items);
+
 	const renderItems = (): JSX.Element => {
 		if (items && items.length) {
 			return (
 				<div className={styles.list}>
-					{items.map(item => (
-						<GarderobCard key={item.id} card={item} isPrimary={true} />
-					))}
+					{items.map(item => {
+						let foundItem = allItems.find(card => card.id === item.id);
+						return <GarderobCard key={item.id} card={foundItem ? foundItem : item} isPrimary={true} />;
+					})}
 				</div>
 			);
 		} else {
